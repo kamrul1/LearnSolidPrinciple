3315551284d7bcfceb09c15dd16c6d56a022c8d2
kamrul.islam <kamrul.islam@dotnet-specialist.co.uk>
2021-10-07 21:25:36 +0100

    Add project files.

---
+++ LearnSolidPrinciple.Rating/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LearnSolidPrinciple.Rating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var engine = new RatingEngine();
+            engine.Rate();
+
+            if (engine.Rating > 0)
+            {
+                Console.WriteLine($"Rating:{engine.Rating}"); ;
+            } 
+            else
+            {
+                Console.WriteLine("No rating produced");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# Learn Solid Principle
+
+This is project where I follow along the practices from the excellent PluralSight
+course by Aralis.  The repo that was used in the course can be found at Github:
+
+[ardalis/SolidSample](https://github.com/ardalis/SolidSample)
+
+# Setup
+
+I copied the basic code at the begin of the course as my begining master branch.
+
+
+
+
+
